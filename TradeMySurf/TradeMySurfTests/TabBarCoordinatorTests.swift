2e1252d7dd2031059cac4c74f3d2ad0682b35a41
Marina Huber <huber.marina@gmail.com>
2019-10-28 08:48:49 +0100

    ADD test to fix for empty coordinator

--- TradeMySurf/TradeMySurfTests/TabBarCoordinatorTests.swift
+++ TradeMySurf/TradeMySurfTests/TabBarCoordinatorTests.swift
@@ -9,22 +9,74 @@
 import XCTest
 @testable import TradeMySurf
 
-class TabBarCoordinatorTests: XCTestCase {
-	var sut: TabBarCoordinator!
-	var navigationController: UINavigationController!
+final class TabBarCoordinatorTests: XCTestCase {
+	typealias CoordinatorType = TabBarCoordinator
+	var coordinator: CoordinatorType!
 	var tabBarController: UITabBarController!
 
 	override func setUp() {
-		navigationController = UINavigationController()
-		tabBarController = UITabBarController()
-		sut = TabBarCoordinator(tabBarController: tabBarController, nav: navigationController)
+		coordinator = CoordinatorType(tabBarController: UITabBarController(), nav:UINavigationController())
 	}
 
-	func test_viewDidLoad_navigation_push_reviewsViewController() {
-		sut.start()
-		XCTAssertEqual(sut.tabBarController, tabBarController)
-		XCTAssertTrue((sut.tabBarController?.isKind(of: TutorialViewController.self))!
-		)
+	override func tearDown() {
+		super.tearDown()
+		coordinator = nil
 	}
+	func testCoordinatorCreation() {
+		//XCTAssertTrue(coordinator.viewControllers.isEmpty)
+		XCTAssertTrue(coordinator.childCoordinators.isEmpty)
+	}
+
+	func testCoordinatorPushViewController() {
+		coordinator.start()
+
+		XCTAssertFalse(coordinator.tabBarController!.isKind(of: TutorialViewController.self))
+	}
+
+	func testCoordinatorPopViewController() {
+		coordinator.start()
+		XCTAssertEqual(coordinator.tabBarController!.viewControllers!.count, 3)
+		coordinator.childCoordinators.popLast()
+
+		waitForUIKitNavigationAnimation(completion: {
+			XCTAssertEqual(self.coordinator.tabBarController!.viewControllers!.count, 3)
+		//	XCTAssertTrue(self.coordinator.tabBarController!.viewControllers!.first is TutorialViewController)
+		})
+	}
+
+
+	func testAddChildCoordinator() {
+		let childCoordinator: CoordinatorType = CoordinatorType(tabBarController: UITabBarController(), nav: UINavigationController())
+		coordinator.addChildCoordinator(coordinator: childCoordinator)
+		XCTAssertFalse(coordinator.childCoordinators.isEmpty)
+	}
+
 
 }
+
+extension XCTestCase {
+    func waitForUIKitNavigationAnimation(completion: @escaping () -> Void) {
+        let expectation: XCTestExpectation = XCTestExpectation(description: #function)
+        let result: XCTWaiter.Result = XCTWaiter.wait(for: [expectation], timeout: 1.5)
+
+        switch result {
+        case .completed:
+            break
+
+        case .timedOut:
+            completion()
+
+        case .incorrectOrder:
+            break
+
+        case .invertedFulfillment:
+            break
+
+        case .interrupted:
+            break
+		@unknown default:
+			break
+		}
+    }
+}
+
