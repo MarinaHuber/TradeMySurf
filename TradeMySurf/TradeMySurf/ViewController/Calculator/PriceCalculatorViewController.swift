eb56ea4ea0e8e64d702b71ddc8efa5ae26f1bc13
Marina Huber <huber.marina@gmail.com>
2019-12-07 00:25:19 +0100

    Add coreML with surf fields in model

--- TradeMySurf/TradeMySurf/ViewController/Calculator/PriceCalculatorViewController.swift
+++ TradeMySurf/TradeMySurf/ViewController/Calculator/PriceCalculatorViewController.swift
@@ -15,31 +15,31 @@ class PriceCalculatorViewController: UIViewController {
 
     @IBOutlet var stackView: UIStackView!
     @IBOutlet var model: UISegmentedControl!
-    @IBOutlet var upgrades: UISegmentedControl!
+    @IBOutlet var gear: UISegmentedControl!
     @IBOutlet var mileageLabel: UILabel!
-    @IBOutlet var mileage: UISlider!
+    @IBOutlet var production: UISlider!
     @IBOutlet var condition: UISegmentedControl!
     @IBOutlet var valuation: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-//        stackView.setCustomSpacing(30, after: model)
-//        stackView.setCustomSpacing(30, after: upgrades)
-//        stackView.setCustomSpacing(30, after: mileage)
-//        stackView.setCustomSpacing(60, after: condition)
+        stackView.setCustomSpacing(30, after: model)
+        stackView.setCustomSpacing(30, after: gear)
+        stackView.setCustomSpacing(30, after: production)
+        stackView.setCustomSpacing(60, after: condition)
 
-//        calculateValue(self)
+        calculateValue(self)
     }
 
     @IBAction func calculateValue(_ sender: Any) {
         let formatter = NumberFormatter()
         formatter.numberStyle = .decimal
         formatter.maximumFractionDigits = 0
-        let formattedMileage = formatter.string(for: mileage.value) ?? "0"
+        let formattedMileage = formatter.string(for: production.value) ?? "0"
         mileageLabel.text = "MILEAGE (\(formattedMileage) miles)"
 
-        if let prediction = try? surfBoards.prediction(model: Double(model.selectedSegmentIndex), premium: Double(upgrades.selectedSegmentIndex), mileage: Double(mileage.value), condition: Double(condition.selectedSegmentIndex)) {
+        if let prediction = try? surfBoards.prediction(model: Double(model.selectedSegmentIndex), gear: Double(gear.selectedSegmentIndex), production: Double(production.value), condition: Double(condition.selectedSegmentIndex)) {
             let clampedValuation = max(2000, prediction.price)
             formatter.numberStyle = .currency
             valuation.text = formatter.string(for: clampedValuation)
@@ -51,4 +51,3 @@ class PriceCalculatorViewController: UIViewController {
 }
 
 extension PriceCalculatorViewController: StoryboardProtocol {}
-
