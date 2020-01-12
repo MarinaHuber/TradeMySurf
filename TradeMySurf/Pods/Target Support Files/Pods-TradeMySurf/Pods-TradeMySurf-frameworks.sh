a33226fc31b9a659ddcb1b84fbb79135afb3b7ce
Marina Huber <huber.marina@gmail.com>
2020-01-12 16:02:23 +0100

    Add SwiftEntryKit for alerts

--- TradeMySurf/Pods/Target Support Files/Pods-TradeMySurf/Pods-TradeMySurf-frameworks.sh
+++ TradeMySurf/Pods/Target Support Files/Pods-TradeMySurf/Pods-TradeMySurf-frameworks.sh
@@ -161,10 +161,14 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/QuickLayout/QuickLayout.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftEntryKit/SwiftEntryKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftyPickerPopover/SwiftyPickerPopover.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/lottie-ios/Lottie.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/QuickLayout/QuickLayout.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftEntryKit/SwiftEntryKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftyPickerPopover/SwiftyPickerPopover.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/lottie-ios/Lottie.framework"
 fi
---
+++ TradeMySurf/Pods/Target Support Files/Pods-TradeMySurfTests/Pods-TradeMySurfTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TradeMySurfTests : NSObject
+@end
+@implementation PodsDummy_Pods_TradeMySurfTests
+@end
