7d7d26b44f65eba398e84f5a094dbc293ca9feca
Marina Huber <huber.marina@gmail.com>
2020-03-07 17:21:35 +0100

    Loading nib with icons tab

--- TradeMySurf/TradeMySurf/SupportingViews/TabItemView.swift
+++ TradeMySurf/TradeMySurf/SupportingViews/TabItemView.swift
@@ -11,8 +11,10 @@ import UIKit
 class TabItemView: UIView {
 
     // MARK: - Outlets
-    let iconTab          = UIImageView()
-    let titleLabel       = TitleLabel(textAlignment: .center, fontSize: 9)
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet private weak var textLabel: UILabel!
+    
+    var contentView: UIView!
     
     let padding: CGFloat = 0
 
@@ -35,17 +37,16 @@ class TabItemView: UIView {
 
     required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-         self.commonInit()
     }
     
     func commonInit() {
         self.backgroundColor = .clear
-        self.titleLabel.textColor = .white
-        self.titleLabel.isHidden = true
-        self.titleLabel.text = nil
-        
         self.configureIconView()
-        
+        self.textLabel.textColor = .white
+        self.textLabel.font = UIFont.boldSystemFont(ofSize: 9)
+        self.textLabel.isHidden = true
+        self.textLabel.text = nil
+            
     }
 
     // MARK: - Configuration
@@ -59,33 +60,40 @@ class TabItemView: UIView {
     // MARK: - Helper
     private func setSelected(_ selected: Bool) {
 
-        self.titleLabel.isHidden = !selected
-        self.titleLabel.alpha = selected ? 1.0 : 0.0
-        self.titleLabel.text = self.tabbarEntry?.title
-        self.iconTab.image = selected ? self.tabbarEntry?.selectedImage : self.tabbarEntry?.image
+        self.textLabel.isHidden = !selected
+        self.textLabel.alpha = selected ? 1.0 : 0.0
+        self.textLabel.text = self.tabbarEntry?.title
+        self.iconImageView.image = selected ? self.tabbarEntry?.selectedImage : self.tabbarEntry?.image
     }
     
     func configureIconView() {
-        iconTab.translatesAutoresizingMaskIntoConstraints = false
-        let stackView = UIStackView(arrangedSubviews: [iconTab, titleLabel])
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.axis = .horizontal
-        stackView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        addSubview(stackView)
-        stackView.contrainToSuperviewEdges()
-//        guard let superview = self.superview else {
-//            return
-//        }
-//        superview.translatesAutoresizingMaskIntoConstraints = false
-//        self.frame = superview.bounds
-//
-//        NSLayoutConstraint.activate([
-//            stackView.leadingAnchor.constraint(equalTo: superview.leadingAnchor, constant: 0.0),
-//            stackView.trailingAnchor.constraint(equalTo: superview.trailingAnchor, constant: 0.0),
-//            stackView.topAnchor.constraint(equalTo: superview.topAnchor, constant: 3),
-//            stackView.bottomAnchor.constraint(equalTo: superview.bottomAnchor, constant: -3)
-//
-//        ])
+
+        self.contentView = self.loadFromNib(withName: String(describing: type(of: self)).components(separatedBy: ".").first!, owner: self, bundle: Bundle.main)
+        self.contentView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        self.addSubview(self.contentView)
+        
+        self.contentView.contrainToSuperviewEdges()
+        self.contentView.backgroundColor = .clear
+        
+    }
+    
+     func loadFromNib(withName nibName: String, owner: AnyObject? = nil, bundle: Bundle? = nil) -> UIView {
+
+        let currentBundle: Bundle = {
+
+            if let bundle = bundle {
+
+                return bundle
+            }
+
+            return Bundle.main
+        }()
+
+        guard let view: UIView = currentBundle.loadNibNamed(nibName, owner: owner, options: nil)?.first as? UIView else {
+            fatalError("UIView \(nibName) not found in Bundle \(currentBundle)")
+        }
+
+        return view
     }
     
 }
