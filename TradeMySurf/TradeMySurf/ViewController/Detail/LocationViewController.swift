7b8b4089315bbaeb417f18908eaaa2694d70b35b
MarinA Huber <marina.huber@bellabeat.com>
2020-12-22 13:01:16 +0100

    Remove GoogleMaps

--- TradeMySurf/TradeMySurf/ViewController/Detail/LocationViewController.swift
+++ TradeMySurf/TradeMySurf/ViewController/Detail/LocationViewController.swift
@@ -7,20 +7,20 @@
 //
 
 import UIKit
-import GoogleMaps
+import MapKit
 
-class LocationViewController: UIViewController, StoryboardProtocol, GMSMapViewDelegate {
+class LocationViewController: UIViewController, StoryboardProtocol {
     @IBOutlet var gradientViewHidden: UIView!
-    @IBOutlet var mapView: GMSMapView!
+    @IBOutlet var mapView: MKMapView?
     private var queryLocation: String = ""
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchGooglePlaces()
-        mapView.delegate = self
-        mapView.isMyLocationEnabled = true
-        mapView.settings.myLocationButton = true
+//        mapView.delegate = self
+//        mapView.isMyLocationEnabled = true
+//        mapView.settings.myLocationButton = true
         
     }
     func fetchGooglePlaces() {
@@ -37,28 +37,23 @@ class LocationViewController: UIViewController, StoryboardProtocol, GMSMapViewDe
     }
     
     func showGooglePlaces(_ mapModel: MapModel) {
-        let markers = mapModel.results.map {
-            let marker = GMSMarker()
-//            marker.position = CLLocationCoordinate2D(latitude: $0.geometry.location.lat, longitude: $0.geometry.location.lng)
-            let camera = GMSCameraPosition.camera(withLatitude: $0.geometry.location.lat, longitude: $0.geometry.location.lng, zoom: 10.0)
-
-            mapView.camera = camera
-            marker.title = $0.name
-            marker.snippet = "Surf stuff"
-            marker.opacity = 0.7
-            marker.map = mapView
-            marker.icon = UIImage(named: "logo_wave")
-            marker.iconView?.sizeThatFits(CGSize(width: 40, height: 40))
-            marker.appearAnimation = .pop
-            
-        }
+//        let markers = mapModel.results.map {
+//            let marker = GMSMarker()
+////            marker.position = CLLocationCoordinate2D(latitude: $0.geometry.location.lat, longitude: $0.geometry.location.lng)
+//            let camera = GMSCameraPosition.camera(withLatitude: $0.geometry.location.lat, longitude: $0.geometry.location.lng, zoom: 10.0)
+//
+//            mapView.camera = camera
+//            marker.title = $0.name
+//            marker.snippet = "Surf stuff"
+//            marker.opacity = 0.7
+//            marker.map = mapView
+//            marker.icon = UIImage(named: "logo_wave")
+//            marker.iconView?.sizeThatFits(CGSize(width: 40, height: 40))
+//            marker.appearAnimation = .pop
+//
+//        }
         
     }
     
-    //TODO:  infoWindow custom here
-    
-    func mapView(_ mapView: GMSMapView, markerInfoWindow marker: GMSMarker) -> UIView? {
-       return UIView()
-    }
-    
+
 }
