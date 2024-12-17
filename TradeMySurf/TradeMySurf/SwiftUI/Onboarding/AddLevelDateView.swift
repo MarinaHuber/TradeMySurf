0c36bfc8806f46cec6edc73095264421f1f32872
marina <huber.marina@gmail.com>
2024-12-17 14:16:09 +0100

    REFACTOR back button from Main

--- TradeMySurf/TradeMySurf/SwiftUI/Onboarding/AddLevelDateView.swift
+++ TradeMySurf/TradeMySurf/SwiftUI/Onboarding/AddLevelDateView.swift
@@ -9,6 +9,177 @@
 import Foundation
 import SwiftUI
 
+protocol ArrowPopoverDelegate {
+    func resetPopoverState()
+}
+
+struct AddLevelDateView: View {
+    @State private var navigateToNext = false
+    @State private var resetTrigger = false
+
+    var body: some View {
+        VStack(spacing: 0) {
+            ArrowPopoverView(navigateToNext: $navigateToNext)
+                .id(resetTrigger) // Force reset if triggered
+        }
+        .edgesIgnoringSafeArea(.all)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color(.pastelPrimary))
+        .fullScreenCover(isPresented: $navigateToNext) {
+            SummaryView()
+        }
+        // Monitor state change of navigateToNext
+        .onChange(of: navigateToNext) { _ , next in
+            if next {
+                DispatchQueue.main.async {
+                    resetState() // Trigger reset before the modal disappears
+                }
+            }
+        }
+    }
+
+    private func resetState() {
+        resetTrigger.toggle()
+    }
+}
+
+struct ArrowPopoverView: View {
+    @Binding var navigateToNext: Bool
+    @State var level: String = Level.beginner.rawValue
+    @State private var showDatePicker = false
+    @State var date = Date()
+    @EnvironmentObject private var themeManager: ThemeManager
+    @State private var showPopover = false
+    @State var showButton: Bool = false
+
+    var body: some View {
+        VStack(spacing: 16) {
+            PickerPopoverView(
+                selectedLevel: $level,
+                selectedDate: $date,
+                showDatePicker: showDatePicker
+            )
+            .opacity(showPopover ? 1 : 0)
+
+            AnimatedTextView()
+                .padding(.top, 10)
+
+            ButtonAnimateColor(title: showDatePicker ? "Done" : "Continue", action: {
+                if showDatePicker {
+                    UserDefaults.standard.selectedDate = date
+                    navigateToNext = true
+                } else {
+                    showDatePicker = true
+                    UserDefaults.standard.selectedLevel = level
+                }
+            })
+            .font(themeManager.selectedTheme.bodyTextFont)
+            .frame(width: 300, height: 40, alignment: .center)
+            .padding([.horizontal, .top])
+            .opacity(showButton ? 1 : 0)
+
+            Spacer()
+        }
+        .onAppear {
+            withAnimation(.easeInOut(duration: 4.0)) {
+                showPopover = true
+                showDatePicker = false
+            }
+        }
+        .onChange(of: level) { _ , _ in
+            showButton = true
+        }
+    }
+}
+
+
+struct PickerPopoverView: View {
+    @Binding var selectedLevel: String
+    @Binding var selectedDate: Date
+    var showDatePicker = false
+    @EnvironmentObject private var themeManager: ThemeManager
+    @State private var levels = [
+        Level.beginner.rawValue,
+        Level.beginnerIntermediate.rawValue,
+        Level.intermediate.rawValue,
+        Level.advanced.rawValue,
+        Level.areals.rawValue,
+        Level.longboarding.rawValue
+    ]
+
+    var body: some View {
+        VStack(spacing: 0) {
+            VStack(spacing: 0) {
+                if showDatePicker {
+                    DatePicker("Select Date", selection: $selectedDate, displayedComponents: .date)
+                        .datePickerStyle(.wheel)
+                        .labelsHidden()
+                        .pickerStyle(.wheel)
+                } else {
+                    Picker(selection: $selectedLevel, label: EmptyView()) {
+                        ForEach(levels, id: \.self) { level in
+                            Text(level)
+                                .font(themeManager.selectedTheme.pickerFont)
+                                .lineLimit(2)
+                                .fixedSize(horizontal: false, vertical: true)
+                                .multilineTextAlignment(.center)
+                        }
+                    }
+                    .pickerStyle(.wheel)
+                }
+            }
+            .background(.pastelSecondary)
+            .cornerRadius(12)
+            .frame(width: 300)
+            .offset(y: 2)
+            .zIndex(1)
+
+            Image(systemName: "arrowtriangle.down.fill")
+                .foregroundColor(.pastelSecondary)
+        }
+        .compositingGroup()
+        .shadow(color: .black.opacity(0.4), radius: 30)
+        .padding(.top, UIScreen.main.bounds.height / 4)
+    }
+}
+
+
+struct AnimatedTextView: View {
+    @State private var showText = false
+    @EnvironmentObject private var themeManager: ThemeManager
+
+    var body: some View {
+        VStack(spacing: 10) {
+                // Define the main message as a single variable
+            let mainMessage = "To assist you with surf\ntherapy and retreats, choose your\n goal and your travel date\nplease"
+
+            let baseTextView = Text(mainMessage)
+                .font(themeManager.selectedTheme.textTitleFont)
+                .foregroundColor(.white)
+                .multilineTextAlignment(.center)
+                .opacity(showText ? 1 : 0)
+
+            if #available(iOS 18, *) {
+                    // iOS 18 and above with LineByLineEffect
+                baseTextView
+                    .textRenderer(LineByLineEffect(
+                        elapsedTime: showText ? 4.0 : 0,
+                        totalDuration: 4.0
+                    ))
+            } else {
+                    // iOS 17 and below with regular Text
+                baseTextView
+                    .padding(.top, 30)
+            }
+        }
+        .onAppear {
+            withAnimation(.easeInOut(duration: 2.0)) {
+                showText = true
+            }
+        }
+    }
+}
+
 struct LineByLineEffect: TextRenderer {
     var elapsedTime: TimeInterval
     var elementDuration: TimeInterval
@@ -75,152 +246,6 @@ extension Text.Layout {
 
     var flattenedRunSlices: some RandomAccessCollection<Text.Layout.RunSlice> {
         flattenedRuns.flatMap(\.self)
-    }
-}
-struct AddLevelDateView: View {
-    @State private var navigateToNext = false
-
-    var body: some View {
-            VStack(spacing: 0) {
-                ArrowPopoverView(navigateToNext: $navigateToNext)
-            }
-            .edgesIgnoringSafeArea(.all)
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(Color(.pastelPrimary))
-            .fullScreenCover(isPresented: $navigateToNext) {
-                SummaryView()
-            }
-    }
-}
-
-import SwiftUI
-
-struct AnimatedTextView: View {
-    @State private var showText = false
-    @EnvironmentObject private var themeManager: ThemeManager
-
-    var body: some View {
-        VStack(spacing: 10) {
-                // Define the main message as a single variable
-            let mainMessage = "To assist you with surf\ntherapy and retreats, choose your\n goal and your travel date\nplease"
-
-            let baseTextView = Text(mainMessage)
-                .font(themeManager.selectedTheme.textTitleFont)
-                .foregroundColor(.white)
-                .multilineTextAlignment(.center)
-                .opacity(showText ? 1 : 0)
-
-            if #available(iOS 18, *) {
-                    // iOS 18 and above with LineByLineEffect
-                baseTextView
-                    .textRenderer(LineByLineEffect(
-                        elapsedTime: showText ? 4.0 : 0,
-                        totalDuration: 4.0
-                    ))
-            } else {
-                    // iOS 17 and below with regular Text
-                baseTextView
-                    .padding(.top, 30)
-            }
-        }
-        .onAppear {
-            withAnimation(.easeInOut(duration: 2.0)) {
-                showText = true
-            }
-        }
-    }
-}
-
-struct ArrowPopoverView: View {
-    @Binding var navigateToNext: Bool
-    @State var level: String = Level.beginner.rawValue
-    @State private var showDatePicker = false
-    @State var date = Date()
-    @EnvironmentObject private var themeManager: ThemeManager
-    @State private var showPopover = false
-    @State var showButton: Bool = false
-
-    var body: some View {
-        VStack(spacing: 16) {
-            PickerPopoverView(selectedLevel: $level, selectedDate: $date, showDatePicker: showDatePicker) // TODO- Add viewModel @ObservableObject instead
-                .opacity(showPopover ? 1 : 0)
-
-            AnimatedTextView()
-                .padding(.top, 10)
-
-            ButtonAnimateColor(title: showDatePicker ? "Done" : "Continue", action: {
-                if showDatePicker {
-                    UserDefaults.standard.selectedDate = date
-                    navigateToNext = true
-                } else {
-                    showDatePicker = true
-                    UserDefaults.standard.selectedLevel = level
-                }
-            })
-            .font(themeManager.selectedTheme.bodyTextFont)
-            .frame(width: 300, height: 40, alignment: .center)
-            .padding([.horizontal, .top]) // Add some spacing at the bottom
-            .opacity(showButton ? 1 : 0)
-
-
-            Spacer()
-        }
-        .onAppear {
-            withAnimation(.easeInOut(duration: 4.0)) {
-                showPopover = true
-                showDatePicker = false
-
-            }
-        }
-        .onChange(of: level, {
-            showButton = true
-        })
-
-    }
-}
-
-struct PickerPopoverView: View {
-    @Binding var selectedLevel: String
-    @Binding var selectedDate: Date
-    var showDatePicker = false
-    @EnvironmentObject private var themeManager: ThemeManager
-    @State private var levels = [Level.beginner.rawValue, Level.beginnerIntermediate.rawValue, Level.intermediate.rawValue, Level.advanced.rawValue, Level.areals.rawValue, Level.longboarding.rawValue]
-
-    var body: some View {
-        VStack(spacing: 0) {
-
-            VStack(spacing: 0) {
-                if showDatePicker {
-                    DatePicker("Select Date", selection: $selectedDate, displayedComponents: .date)
-                        .datePickerStyle(.wheel)
-                        .labelsHidden()
-                        .pickerStyle(.wheel)
-                } else {
-                    Picker(selection: $selectedLevel, label: EmptyView()) {
-                        ForEach(levels, id: \.self) { level in
-                            Text(level)
-                                .font(themeManager.selectedTheme.pickerFont)
-                                .lineLimit(2)
-                                .fixedSize(horizontal: false, vertical: true)
-                                .multilineTextAlignment(.center)
-                        }
-                    }
-                    .pickerStyle(.wheel)
-                }
-            }
-            .background(.pastelSecondary)
-            .cornerRadius(12)
-            .frame(width: 300)
-            .offset(y: 2)
-            .zIndex(1)
-
-            Image(systemName: "arrowtriangle.down.fill") // arrow
-                .foregroundColor(.pastelSecondary)
-        }
-        .compositingGroup()
-        .shadow(color: .black.opacity(0.4), radius: 30)
-        .padding(.top, UIScreen.main.bounds.height / 4)
-
     }
 }
 
--- TradeMySurf/TradeMySurf/SwiftUI/SwiftUIViews/AlertButtonView.swift
+++ TradeMySurf/TradeMySurf/SwiftUI/SwiftUIViews/AlertButtonView.swift
@@ -14,37 +14,35 @@ struct AlertButtonView: View {
 
     var body: some View {
         HStack(alignment: .center, spacing: 20) {
-            Group {
                 Button(action: {
                     navigationPath.append(NavigationOption.welcome)
                 }) {
                     Text("Back")
-                        .bold()
-                        .font(themeManager.selectedTheme.bodyTextFont)
                         .foregroundColor(.red)
-                        .frame(width: 100, height: 40)
                         .padding(.horizontal, 8)
+                        .frame(width: 100, height: 40)
                         .overlay(
                             RoundedRectangle(cornerRadius: 10)
                                 .stroke(.red, lineWidth: 1)
                         )
+
                 }
-                
+
                 Button(action: {
                     navigationPath.append(NavigationOption.main)
                 }) {
                     Text("Match me")
-                        .bold()
-                        .font(themeManager.selectedTheme.bodyTextFont)
                         .foregroundColor(.white)
-                        .frame(width: 100, height: 40)
-                        .overlay(
+                        .frame(width: 100, height: 40) // Define button size
+                        .padding(.horizontal, 8)       // Apply padding after defining size
+                        .overlay(                      // Overlay with border on top
                             RoundedRectangle(cornerRadius: 10)
                                 .stroke(.white, lineWidth: 1)
                         )
                 }
-            }
         }
+        .font(themeManager.selectedTheme.bodyTextFont)
+
     }
 }
 
